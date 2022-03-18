43f1e07953a14e0a274ed115d82365e6e281c26c
fangt11 <fangt11@ke.com>
2022-03-18 11:37:12 +0800

    fix: 路径

--- deploy-gh.js
+++ deploy-gh.js
@@ -2,4 +2,4 @@ const { readFileSync, writeFileSync } = require('fs');
 
 const content = readFileSync('docs-dist/index.html').toString();
 
-writeFileSync('index.html', content.replace(/\"\/umi\./g, '"https://raw.githubusercontent.com/shuoshubao/components/master/docs-dist/umi.'))
+writeFileSync('index.html', content.replace(/\"\/umi\./g, '"./docs-dist/umi.'))
