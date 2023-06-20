2be1f108b0deb6250a1e953fe992689f37a8cb04
shuoshubao <shuoshubao@gmail.com>
2023-06-20 10:30:47 +0800

    feat: 升级@nbfe/react-color,@nbfe/tools; 增加 esm

--- rollup.config.js
+++ rollup.config.js
@@ -1,18 +1,35 @@
 import babel from '@rollup/plugin-babel'
 import scss from 'rollup-plugin-scss'
 
-export default {
-  input: 'lib/index.js',
-  output: {
-    file: 'dist/index.js',
-    format: 'cjs'
+export default [
+  {
+    input: 'lib/index.js',
+    output: {
+      file: 'dist/index.js',
+      format: 'cjs'
+    },
+    plugins: [
+      scss({
+        fileName: 'index.css'
+      }),
+      babel({
+        presets: ['@babel/preset-env', '@babel/preset-react']
+      })
+    ]
   },
-  plugins: [
-    scss({
-      fileName: 'index.css'
-    }),
-    babel({
-      presets: ['@babel/preset-env', '@babel/preset-react']
-    })
-  ]
-}
+  {
+    input: 'lib/index.js',
+    output: {
+      file: 'dist/index.esm.js',
+      format: 'esm'
+    },
+    plugins: [
+      scss({
+        fileName: 'index.css'
+      }),
+      babel({
+        presets: ['@babel/preset-env', '@babel/preset-react']
+      })
+    ]
+  }
+]
